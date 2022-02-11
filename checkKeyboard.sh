543b1e07427b42cfd47bbf1f9810e14181610cdf
cheyer <cheyer03@gmail.com>
2022-02-11 16:15:18 +0100

    Add script to workflow

---
+++ checkKeyboard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export FILE="page.html"
+export PATTERN="White Backlight / Low Profile Keychron Optical (Hot-swappable) / Brown - Sold out"
+export URL="https://www.keychron.com/products/keychron-k3-wireless-mechanical-keyboard/?variant=32220198797401"
+
+curl $URL > $FILE &&
+
+if cat $FILE | grep "$PATTERN"; then
+    echo found
+else
+    echo not found
+fi
